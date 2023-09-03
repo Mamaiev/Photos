b0df32275609e31ebabe2f5a8792ef4f1a035db1
mac <Mamaiev>
2023-09-03 15:46:03 +0300

    adds check for no response from springboot application

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,25 +1,27 @@
-import React, { useContext, useEffect } from "react";
-import { PhotoContext } from "../context/PhotoContext";
+import React, {useContext, useEffect} from "react";
+import {PhotoContext} from "../context/PhotoContext";
 import Gallery from "./Gallery";
 import Loader from "./Loader";
 import Map from "./Map";
+import NoPhotos from "./NoPhotos";
 
-const Container = ({ searchTerm }) => {
-  const { photos, loading, runSearch } = useContext(PhotoContext);
-  useEffect(() => {
-    runSearch(searchTerm);
-    // eslint-disable-next-line
-  }, [searchTerm]);
+const Container = ({searchTerm}) => {
+    const {photos, loading, runSearch} = useContext(PhotoContext);
+    useEffect(() => {
+        runSearch(searchTerm);
+        // eslint-disable-next-line
+    }, [searchTerm]);
 
-  return (
-    <div className="photo-container">
-      {loading ? <Loader /> :
-          <div>
-            {/*<Gallery data={photos} />*/}
-            <Map data={photos}/>
-          </div>}
-    </div>
-  );
+    return (
+        <div className="photo-container">
+            {loading ? <NoPhotos/> :
+                <div>
+                    {/*<Gallery data={photos} />*/}
+                    <Map data={photos}/>
+                </div>
+            }
+        </div>
+    );
 };
 
 export default Container;
--- src/context/PhotoContext.js
+++ src/context/PhotoContext.js
@@ -1,31 +1,37 @@
-import React, { createContext, useState } from "react";
+import React, {createContext, useState} from "react";
 import axios from "axios";
+
 export const PhotoContext = createContext();
 
 const PhotoContextProvider = props => {
-  const [photos, setPhotos] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const runSearch = query => {
-    axios
-      .get(
-        `http://localhost:8080/all`
-      )
-      .then(response => {
-        setPhotos(response.data);
-        setLoading(false);
-      })
-      .catch(error => {
-        console.log(
-          "Encountered an error with fetching and parsing data",
-          error
-        );
-      });
-  };
-  return (
-    <PhotoContext.Provider value={{ photos, loading, runSearch }}>
-      {props.children}
-    </PhotoContext.Provider>
-  );
+    const [photos, setPhotos] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const runSearch = query => {
+        axios
+            .get(`http://localhost:8080/all`, {
+                    timeout: 5000
+                }
+            )
+            .then(response => {
+                setPhotos(response.data);
+                setLoading(false);
+            })
+            .catch(error => {
+                if (error.code !== 200) {
+                    console.log('Request timeout! Please get request again.');
+                } else {
+                    console.log(
+                        "Encountered an error with fetching and parsing data",
+                        error
+                    );
+                }
+            });
+    };
+    return (
+        <PhotoContext.Provider value={{photos, loading, runSearch}}>
+            {props.children}
+        </PhotoContext.Provider>
+    );
 };
 
 export default PhotoContextProvider;
